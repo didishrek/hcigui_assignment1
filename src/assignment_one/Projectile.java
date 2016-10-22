2df26ba505e07242a9e444f60479142854f6c71e
AdrienS <didishrek80@gmail.com>
2016-10-22 16:05:04 +0100

    finish

--- src/assignment_one/Projectile.java
+++ src/assignment_one/Projectile.java
@@ -9,6 +9,9 @@ package assignment_one;
     Options :
         - Can only write integer or double in mass_textField
         - Button color with css.
+        - Add chart
+        - I add more layout to make a better view
+        - Add a minimum size for the window.
  */
 
 import javafx.application.Application;
@@ -16,10 +19,16 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.text.DecimalFormat;
@@ -31,9 +40,20 @@ public class Projectile extends Application {
     private final String HUMAN = "Adult Human";
     private final String PIANO = "Piano";
 
-    //Layout
+    private Label resultLabel = new Label("Results : ");
+
+    //Chart
+    private NumberAxis xAxis = new NumberAxis();
+    private NumberAxis yAxis = new NumberAxis();
+    private LineChart<Number,Number> chart = new LineChart<Number,Number>(xAxis,yAxis);
+    private XYChart.Series series = new XYChart.Series();
+
+    //Layouts
+    private HBox mainLayout = new HBox();
     private GridPane gp = new GridPane();
     private HBox hbox_speed = new HBox();
+    private VBox results = new VBox();
+    private GridPane result = new GridPane();
 
     //Projectile Type
     private Label projectile_type_label = new Label("Projectile Type");
@@ -49,6 +69,7 @@ public class Projectile extends Application {
     private Slider angle_slider = new Slider(0, 90, 45);
     private TextField angle_textField = new TextField("45.0");
     private double angle = 45.0;
+    private double angle_rad;
     //Formating the values in the duration box
     DecimalFormat df = new DecimalFormat("###,###.###");
 
@@ -112,40 +133,79 @@ public class Projectile extends Application {
         time_textField.setEditable(false);
 
         //button color and sizes
-        erase_button.setStyle("-fx-background-color: linear-gradient(#ff5940, #ff0c20);" +
-                "-fx-text-fill: #530907;");
+        erase_button.setStyle("-fx-background-color: linear-gradient(#ff5940, #ff0c20);" + "-fx-text-fill: #530907;");
         erase_button.setMaxWidth(Double.MAX_VALUE);
-        fire_button.setStyle("-fx-background-color: linear-gradient(#f0ff35, #a9ff00);" +
-                "-fx-text-fill: #395306;");
+        fire_button.setStyle("-fx-background-color: linear-gradient(#f0ff35, #a9ff00);" + "-fx-text-fill: #395306;");
         fire_button.setMaxWidth(Double.MAX_VALUE);
 
-        //init gridpane
+        /*
+            INITIALISE LAYOUTS
+         */
+
+        //Set column constraints
+        ColumnConstraints col1Constraints = new ColumnConstraints();
+        col1Constraints.setPercentWidth(50);
+        ColumnConstraints col2Constraints = new ColumnConstraints();
+        col1Constraints.setPercentWidth(50);
+
+        //Init GridPane for setting
         gp.addRow(0, projectile_type_label, projectile_type_combobox);
         gp.addRow(1, mass_label, mass_textField);
-        gp.addRow(2, angle_label, angle_textField, angle_slider);
-        gp.addRow(3, initial_speed_label, intitial_speed_textField, hbox_speed);
-        gp.addRow(4, range_label, range_textField);
-        gp.addRow(5, height_label, height_textField);
-        gp.addRow(6, time_label, time_textField);
-        gp.addRow(7, fire_button, erase_button);
+        gp.addRow(2, angle_label, angle_textField);
+        gp.add(angle_slider, 0, 3, 2, 1);
+        gp.addRow(4, initial_speed_label, intitial_speed_textField);
+        gp.add(hbox_speed, 0, 5, 2, 1);
+        gp.addRow(6, fire_button, erase_button);
+        gp.getColumnConstraints().addAll(col1Constraints, col2Constraints);
 
+        // init gridpane for results
+        result.addRow(0, resultLabel);
+        result.addRow(1, range_label, range_textField);
+        result.addRow(2, height_label, height_textField);
+        result.addRow(3, time_label, time_textField);
+        result.getColumnConstraints().addAll(col1Constraints, col2Constraints);
+
+        // init VBox for all results (texfields and chart)
+        results.getChildren().addAll(result, chart);
+
+        // init HBOX containing settings and results
+        mainLayout.getChildren().addAll(gp, results);
+        mainLayout.setSpacing(15);
+        mainLayout.setPadding(new Insets(10, 10, 10, 10));
+
+        //init chart
+        chart.setCreateSymbols(false);  // To display a line rather than points
+        chart.setTitle("Projectile's trajectory");
+        chart.setLegendVisible(false);  // To remove the points legend
+        chart.getData().addAll(series); // Add the series of values
+        chart.setAnimated(false);   // Remove the animation : can be source of bugs
+        xAxis.setLabel("Range (m)");
+        yAxis.setLabel("Height (m)");
+        xAxis.setAutoRanging(false);    // In this case, can be source of weird results so I remove it
+        yAxis.setAutoRanging(false);    // In this case, can be source of weird results so I remove it
+        xAxis.setLowerBound(0.0);
+        yAxis.setLowerBound(0.0);
 
 
         initalizeControlValues();
 
         //  Listener for angle Slider to set angle TextTield and the angle variable
         angle_slider.valueProperty().addListener(new ChangeListener<Number>(){
+            @Override
             public void changed(final ObservableValue<? extends Number> observable, final Number oldValue, final Number newValue){
                 angle_textField.setText(String.valueOf(Math.round( (double)newValue * 100.0 ) / 100.0));
                 fire();
+                drawChart();
             }
         });
 
         // Listener for inital_speed ToggleGroup to set initital_speed TextField
         initial_speed_toggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
             public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle,Toggle new_toggle) {
                 intitial_speed_textField.setText(new_toggle.getProperties().values().toArray()[0].toString());
                 fire();
+                drawChart();
             }
         });
 
@@ -154,6 +214,7 @@ public class Projectile extends Application {
             @Override
             public void handle(ActionEvent event) {
                 fire();
+                drawChart();
             }
         });
 
@@ -162,6 +223,7 @@ public class Projectile extends Application {
             @Override
             public void handle(ActionEvent event) {
                 initalizeControlValues();
+                eraseChart();
             }
         });
 
@@ -170,6 +232,7 @@ public class Projectile extends Application {
             @Override
             public void handle(ActionEvent event) {
                 initalizeControlValues();
+                eraseChart();
             }
         });
 
@@ -193,11 +256,11 @@ public class Projectile extends Application {
     @Override
     public void start(Stage primaryStage) {
         // set a title on the window, set a scene, size, and show the window
-        Scene scene = new Scene(gp, 500, 300);
+        Scene scene = new Scene(mainLayout, 940, 550);
         primaryStage.setTitle(TITLE);
         primaryStage.setScene(scene);
-//        primaryStage.setMinHeight(320);
-//        primaryStage.setMinWidth(500);
+        primaryStage.setMinHeight(570); // set a min height
+        primaryStage.setMinWidth(960);  // set min width
         primaryStage.show();
     }
 
@@ -217,7 +280,7 @@ public class Projectile extends Application {
         mass = Double.parseDouble(mass_textField.getText());
         angle = Double.parseDouble(angle_textField.getText());
         initial_speed = Double.parseDouble(intitial_speed_textField.getText());
-        Double angle_rad = (angle * Math.PI) / 180.0;
+        angle_rad = (angle * Math.PI) / 180.0;
 
         range = (Math.pow(initial_speed, 2) / gravitational_accelleration) * Math.sin(2 * angle_rad);
         time = ((2 * initial_speed) * Math.sin(angle_rad)) / gravitational_accelleration;
@@ -252,6 +315,33 @@ public class Projectile extends Application {
         range_textField.setText("");
         time_textField.setText("");
         height_textField.setText("");
+    }
+
+    //This function erase the charts then redraw it.
+    private void drawChart(){
+        eraseChart();
+
+        //This is because I remove the autoranging
+        yAxis.setUpperBound(height);
+        yAxis.setTickUnit((double)(int)(height / 10)); // Double cast to only get integer
+        xAxis.setUpperBound(range);
+        xAxis.setTickUnit((double)(int)(range / 10)); // Double cast to only get integer
+
+        //Create all serie's points.
+        double t = 0.0; // time
+        double y = 0.0; // coord y
+        double x = 0.0; // coord x
+        while (y >= 0){
+            y = t * initial_speed * Math.sin(angle_rad) - (gravitational_accelleration * Math.pow(t, 2) / 2);
+            x = initial_speed * Math.cos(angle_rad) * t;
+            series.getData().add(new XYChart.Data<>(x, y));
+            t += 0.1;
+        }
+    }
+
+    //This function erase datas in serie and by consequence erase the chart
+    private void eraseChart(){
+        series.getData().clear();
     }
 }
 
